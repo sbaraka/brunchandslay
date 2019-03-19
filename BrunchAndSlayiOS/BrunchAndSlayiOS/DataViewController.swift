5cba4a46d573f51dc6e5decb1093fe55b2a57dee
Noah Stanford <noahstandingford@gmail.com>
2019-03-19 12:27:05 -0400

    Initial setup of Xcode project.

---
+++ BrunchAndSlayiOS/BrunchAndSlayiOS/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  BrunchAndSlayiOS
+//
+//  Created by Noah on 3/19/19.
+//  Copyright © 2019 Brunch and Slay. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
