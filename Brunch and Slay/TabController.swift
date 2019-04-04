3625160734570571ef580b7185cfc5db479bb604
Noah Stanford <noahstandingford@gmail.com>
2019-04-04 19:59:16 -0400

    Added controller classes for the navigation bar view and the tab controller in in case they need custom code. Made changes to the storyboard to make the UI work as intended.

---
+++ Brunch and Slay/TabController.swift
@@ -0,0 +1,19 @@
+//
+//  TabController.swift
+//  Brunch and Slay
+//
+//  Created by Noah on 4/4/19.
+//  Copyright © 2019 Brunch and Slay. All rights reserved.
+//
+
+import UIKit
+
+class TabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
