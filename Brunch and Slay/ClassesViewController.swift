b58165f24fe3737e377c1acaba0e08d3fde0945e
sbaraka <sbaraka@umich.edu>
2019-05-18 16:22:47 -0400

    changed view a bit. Play youtube video

--- Brunch and Slay/ClassesViewController.swift
+++ Brunch and Slay/ClassesViewController.swift
@@ -7,41 +7,17 @@
 //
 
 import UIKit
+import WebKit
 
-class ClassesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    @IBOutlet weak var classesTable: UITableView!
-    
-    var classesTableData: [ClassData] = []
-    
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return classesTableData.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = classesTable.dequeueReusableCell(withIdentifier: "classCell") as! ClassCell
-        
-        cell.classImage.image = classesTableData[indexPath.row].image
-        
-        cell.classTitle.text = classesTableData[indexPath.row].title
-        
-        
-        let backGroundView = UIView()
-        
-        backGroundView.backgroundColor = UIColor.init(displayP3Red: 255/255, green: 147/255, blue: 0/255, alpha: 255/255)
-        
-        cell.selectedBackgroundView = backGroundView
-        
-        return cell
-    }
-    
-    
+class ClassesViewController: UIViewController{
+    @IBOutlet weak var youtubewebview: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getVideo(videoCode: "WR4QQsNm1ok")
     }
-    
-    
+    func getVideo(videoCode:String){
+        let url = URL(string:"https://www.youtube.com/embed/\(videoCode)")
+        youtubewebview.load(URLRequest(url: url!))
+    }
 }
