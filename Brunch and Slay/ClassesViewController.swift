df4b10159d89074d75e83f222bba0e01cd4d8562
sbaraka <sbaraka@umich.edu>
2019-05-19 17:33:31 -0400

    allowing for playing of multiple URLS

--- Brunch and Slay/ClassesViewController.swift
+++ Brunch and Slay/ClassesViewController.swift
@@ -9,15 +9,39 @@
 import UIKit
 import WebKit
 
-class ClassesViewController: UIViewController{
+class YoutubeCell:UITableViewCell{
     @IBOutlet weak var youtubewebview: WKWebView!
+}
+
+
+
+
+class ClassesViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    let list = [URL(string:"https://www.youtube.com/embed/WR4QQsNm1ok"), URL(string:"https://www.youtube.com/embed/2nt8aFDCnNE")]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return list.count
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+//        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "youtubecell")
+//        cell.textLabel?.text = list[indexPath.row]
+//        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "youtubecell") as! YoutubeCell
+        let url = URL(string:"https://www.youtube.com/embed/WR4QQsNm1ok")
+        cell.youtubewebview.load(URLRequest(url: list[indexPath.row]!))
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        getVideo(videoCode: "WR4QQsNm1ok")
+        //getVideo(videoCode: "WR4QQsNm1ok")
     }
-    func getVideo(videoCode:String){
-        let url = URL(string:"https://www.youtube.com/embed/\(videoCode)")
-        youtubewebview.load(URLRequest(url: url!))
-    }
+//    func getVideo(videoCode:String){
+//        let url = URL(string:"https://www.youtube.com/embed/\(videoCode)")
+//        youtubewebview.load(URLRequest(url: url!))
+//    }
 }
